82d40a92b92250a791ac5506f20133f10ff93b29
janakp2411 <janakp2411@gmail.com>
2018-08-12 12:24:34 -0400

    Menu

--- src/components/app/MenuBar/index.js
+++ src/components/app/MenuBar/index.js
@@ -5,19 +5,19 @@ const MenuBar = () => {
     return(
         <div className="row menu-bar-conatiner">
             <div className="col-md-4 menu-col">
-            Beack fast  Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
-            Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
-            Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
+            Break fast  Break fast Break fast Break fast Break fast Break fast Break fast Break fast
+            Break fast Break fast Break fast Break fast Break fast Break fast Break fast Break fast
+            Break fast Break fast Break fast Break fast Break fast Break fast Break fast Break fast
             </div>
             <div className="col-md-4 menu-col">
-            Beack fast  Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
-            Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
-            Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
+            Break fast  Break fast Break fast Break fast Break fast Break fast Break fast Break fast
+            Break fast Break fast Break fast Break fast Break fast Break fast Break fast Break fast
+            Break fast Break fast Break fast Break fast Break fast Break fast Break fast Break fast
             </div>
             <div className="col-md-4 menu-col">
-            Beack fast  Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
-            Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
-            Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
+            Break fast  Break fast Break fast Break fast Break fast Break fast Break fast Break fast
+            Break fast Break fast Break fast Break fast Break fast Break fast Break fast Break fast
+            Break fast Break fast Break fast Break fast Break fast Break fast Break fast Break fast
             </div>
         </div>
     )
