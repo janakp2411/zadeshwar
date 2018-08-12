24015fdfcb88f92df7fb75339c71cb4e4fdf5487
janakp2411 <janakp2411@gmail.com>
2018-08-12 02:03:42 -0400

    Added dummy

--- src/components/app/MenuBar/index.js
+++ src/components/app/MenuBar/index.js
@@ -3,15 +3,21 @@ import './index.css';
 
 const MenuBar = () => {
     return(
-        <div className="row">
+        <div className="row menu-bar-conatiner">
             <div className="col-md-4 menu-col">
-            Beack fast
+            Beack fast  Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
+            Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
+            Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
             </div>
             <div className="col-md-4 menu-col">
-            Lunch
+            Beack fast  Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
+            Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
+            Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
             </div>
             <div className="col-md-4 menu-col">
-            Dinner
+            Beack fast  Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
+            Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
+            Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast Beack fast
             </div>
         </div>
     )
