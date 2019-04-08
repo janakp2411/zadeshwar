828387d50d610886693dc025252d5159024b4a20
janakp2411 <janakp2411@gmail.com>
2019-04-07 23:10:12 -0400

    added

--- server.js
+++ server.js
@@ -18,18 +18,22 @@ const forceSSL = function() {
 // Instruct the app
 // to use the forceSSL
 // middleware
-app.use(forceSSL());
+// app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/zadeshwar'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+// so that PathLocationStrategy can be used
+  console.log('Request comminpppppppppppppg')
+  res.sendFile(path.join(__dirname + '/dist/zadeshwar/index.html'));
 });
 
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, function(){
+    console.log('Server started s')
+});
