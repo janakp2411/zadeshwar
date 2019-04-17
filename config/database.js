1a1665dc73923513591b76a2f61d9d5db340ed16
janakp2411 <30379298+janakp2411@users.noreply.github.com>
2019-04-17 10:54:04 -0400

    update API Key

--- config/database.js
+++ config/database.js
@@ -5,17 +5,15 @@ const backendServer = 'http://localhost:8080';
 const baseUrl = (!isDev) ? domain : frontendServer;
 const serverHost = (!isDev) ? domain : backendServer;
 const fileUpload = (!isDev) ? './public/assets/imgs/' : './angular-src/src/assets/imgs/'
-const GEO_API = "AIzaSyAKI3dAptAxQRezsMG7yIrGYl9BzxqsdSY";
-//process.env.DATA_BASE_KEY = 'mongodb://zadeshwar:zadeshwar123@ds233806.mlab.com:33806/zadeshwar';
+
 module.exports = {
-  // configure the code below with your username, password and mlab database information
-  database: process.env.DATA_BASE_KEY, //'mongodb://zadeshwar:zadeshwar123@ds233806.mlab.com:33806/zadeshwar',   //prod
-  //database: 'mongodb://localhost:27017/meanauth',    //dev
-  secret: 'yoursecret',
-  email: 'janakp2411@gmail.com',
+  database: process.env.DATA_BASE_KEY,
+  GEO_API: process.env.GEO_API,
+  secret: process.env.SECRET_KEY,
+  email:  process.env.EMAIL_NAME,
   baseUrl: baseUrl,
   serverHost: serverHost,
   userRole: 'userRole',
   uIfileUpload: './assets/imgs/',
-  emailAPIkey: "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"
+  emailAPIkey: process.env.EMAIL_API_KEY
 }
