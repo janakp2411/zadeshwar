929e51dccd204e309aef026a24e90bf467c47849
janakp2411 <30379298+janakp2411@users.noreply.github.com>
2019-04-17 10:43:31 -0400

    Update with key

--- config/database.js
+++ config/database.js
@@ -6,10 +6,10 @@ const baseUrl = (!isDev) ? domain : frontendServer;
 const serverHost = (!isDev) ? domain : backendServer;
 const fileUpload = (!isDev) ? './public/assets/imgs/' : './angular-src/src/assets/imgs/'
 const GEO_API = "AIzaSyAKI3dAptAxQRezsMG7yIrGYl9BzxqsdSY";
-
+//process.env.DATA_BASE_KEY = 'mongodb://zadeshwar:zadeshwar123@ds233806.mlab.com:33806/zadeshwar';
 module.exports = {
   // configure the code below with your username, password and mlab database information
-  database: 'mongodb://zadeshwar:zadeshwar123@ds233806.mlab.com:33806/zadeshwar',   //prod
+  database: process.env.DATA_BASE_KEY, //'mongodb://zadeshwar:zadeshwar123@ds233806.mlab.com:33806/zadeshwar',   //prod
   //database: 'mongodb://localhost:27017/meanauth',    //dev
   secret: 'yoursecret',
   email: 'janakp2411@gmail.com',
